f6bc0135684fe4c2dd42c4d798f4be42071b7afd
Oyekanmi Jelili Oyetunji <oyekanmijelili@gmail.com>
2021-06-24 21:34:43 +0100

    ....

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
+
 var express = require('express');
 var app = express();
-
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 3000;
@@ -18,52 +18,101 @@ app.get('/', function(req, res) {
 	res.render('index');
 });
 
-app.listen(port, function() {
-	console.log('Our app is running on http://localhost:' + port);
+//app.use(express.static('public'));
+const ejs = require('ejs');
+app.set('view engine', 'ejs');
+
+/*app.get('/', function(req, res) {
+
+	// ejs render automatically looks in the views folder
+	res.render('/index');
+});*/
+
+
+
+//CODES FOR DOWNLOADING FROM SERVER 
+
+var http = require('http').Server(app);
+
+//This will add button to link to your download page
+//res.sendfile(link of html file in your pc)
+
+app.get('/',function(req,res){ 
+  res.sendFile('C:/Users/Oyekanmi Jelil/jelil-info1/views/index');
+});
+app.get('/download',function(req,res){
+    res.download(__dirname +'/upload_folder/cv.pdf','cv.pdf');
 });
 
-
-
-
-
-//The codes below are codes for sending mail messages to my email address using mailgun
-const sendMail = require('./mail2')
-
-const log = console.log;
-
-const path = require('path');
-
-const PORT = 3000;
-
-
-// Chunk 2
-// Data parsing
-app.use(express.urlencoded({
-     extended: false
-}));
-app.use(express.json());
-
-// email, subject, text
-app.post('/email', (req, res)  => {
-    // TODO:
-    // send email here
-    const { subject, email, text } = req.body;
-    console.log('Data: ', req.body);
-
-    sendMail(email, subject, text, function(err, data) {
-        if (err) {
-          res.status(500).json({message: 'Internal Error'});
-     
-        } else {
-          res.json({message: 'Email sent!!!!'});
-        }
+app.get('/Thanks',function(req,res){
+    res.sendFile('C:/Users/Oyekanmi Jelil/jelil-info1/views/thanksForDownloading.html')//Full path of thankyou.html file
     });
+
+
+    //CODES FOR LINKS
+    app.get('/home', (req, res) => {
+
+      res.render('C:/Users/Oyekanmi Jelil/jelil-info1/views/home');
+      
+      });
+    
+      app.get('/myResume', (req, res) => {
+    
+        res.render('myResume');
+        
+        });
+    //COdes for message to display after downloading
+        app.get('/Thanks', (req, res) => {
+    
+          res.render('thanksForDownloading.html');
+          
+          });
+
+      /*app.listen(port, function() {
+            console.log('Our app is running on http://localhost:' + port);
+        });*/
+
+
+
+
+
+
+
+//CODES FOR FORM DATA
+
+//const express = require("express");
+const mongoose = require("mongoose");
+const foodRouter = require("./routes/foodRoutes.js");
+//const app = express();
+
+//app.use(express.json());
+var bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+
+
+        
+        var path = require('path');
+        
+        var mongodb = require('mongodb');
+        var fs = require('fs');
+        var path = require('path');
+        require('dotenv/config');
+
+
+
+        
+        mongoose.Promise = global.Promise;
+        
+
+    mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.8chyx.mongodb.net/FormData?retryWrites=true&w=majority`,
+     { useNewUrlParser: true, useUnifiedTopology: true,useFindAndModify: false }
+  
+);
+
+app.use(foodRouter);
+
+app.listen(3000, () => {
+  console.log("Server is running...");
 });
-
-
-
-app.get('/', (req, res) => {
-     res.sendFile(path.join(__dirname, 'views','index.ejs' ));
-
-});
-
